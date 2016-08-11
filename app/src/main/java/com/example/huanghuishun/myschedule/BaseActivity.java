ab8763edf5d4c6b4a1b7ebf55dc0948c99692020
huanghuishun <448408196@qq.com>
2016-08-11 14:20:59 +0800

    firstcommit

---
+++ app/src/main/java/com/example/huanghuishun/myschedule/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.huanghuishun.myschedule;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by huanghuishun on 2016/8/11.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+
+}
